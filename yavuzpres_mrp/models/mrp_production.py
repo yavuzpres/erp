fcdf6923d7eeb7956a4184346415c94bf983b21d
yavuzpres <87063348+yavuzpres@users.noreply.github.com>
2023-02-13 12:27:05 +0300

    mrp 2

--- yavuzpres_mrp/models/mrp_production.py
+++ yavuzpres_mrp/models/mrp_production.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class MrpProduction(models.Model):
@@ -6,3 +6,10 @@ class MrpProduction(models.Model):
 
     qty_produced = fields.Float(
         'Produced Quantity', store=True)
+    qty_remaining = fields.Float(
+        'Quantity To Be Produced', compute='_compute_qty_remaining', store=True)
+
+    @api.depends('product_qty', 'qty_produced')
+    def _compute_qty_remaining(self):
+        for prop in self:
+            prop.qty_remaining = (prop.product_qty - prop.qty_produced)
