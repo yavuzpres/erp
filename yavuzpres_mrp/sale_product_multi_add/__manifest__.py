59fbf5ce4414e220426514560ccfd3b00a656845
yavuzpres <erp@yavuzpres.com.tr>
2023-03-03 07:51:55 +0300

    Satın Alma Talep Modülü

---
+++ yavuzpres_mrp/sale_product_multi_add/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Sale Multi Add',
+    'summary': 'Yavuzpres Sale Customization',
+    'version': '13.0.1.0.0',
+    'category': 'Sale',
+    'author': 'Yavuzpres',
+    'website': 'http://www.yavuzpres.com.tr',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    "data": [
+        "wizards/sale_import_products_view.xml",
+        "views/sale_view.xml",
+    ],
+}
